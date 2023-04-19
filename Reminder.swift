b81dcc1a0a8e48ef4c077479ea33f028be07692e
Vladimir <49140851+VladimirCreator@users.noreply.github.com>
2023-04-20 01:58:25 +0500

    Append `Reminder`
    It contains `title`, `subtitle`, and `isDone`.

--- Reminder.swift
+++ Reminder.swift
@@ -1,1 +1,11 @@
+final class Reminder {
+    var title: String
+    var subtitle: String
+    var isDone: Bool
 
+    init(_ title: String, _ subtitle: String, isDone: Bool) {
+        self.title = title
+        self.subtitle = subtitle
+        self.isDone = isDone
+    }
+}
