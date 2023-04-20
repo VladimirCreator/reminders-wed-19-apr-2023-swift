edda0abb055042aa57bcf6a06f7e32f5b87310d1
Vladimir <49140851+VladimirCreator@users.noreply.github.com>
2023-04-20 21:26:42 +0500

    Append `RemindersDelegate`

--- RemindersDelegate.swift
+++ RemindersDelegate.swift
@@ -1,1 +1,9 @@
 import UIKit
+
+final class RemindersDelegate: UIResponder, UIApplicationDelegate {
+    var windows: UIWindow?
+
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        window?.rootViewController = UINavigationController(rootViewController: RemindersViewController())
+    }
+}
