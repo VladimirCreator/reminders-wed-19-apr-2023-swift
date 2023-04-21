7745be712905d78f87012f3b188879b007b8f027
Vladimir <49140851+VladimirCreator@users.noreply.github.com>
2023-04-21 15:06:57 +0500

    Delete `extension RemindersViewController: UICollectionViewDataSource`

--- RemindersViewController.swift
+++ RemindersViewController.swift
@@ -43,24 +43,6 @@ final class RemindersViewController: UIViewController {
     }
 }
 
-// MARK: `UICollectionViewDataSource`
-
-extension RemindersViewController: UICollectionViewDataSource= {
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Int> { cell, indexPath, itemIdentifier in
-            var contentConfiguration = cell.defaultContentConfiguration()
-            contentConfiguration.text = "This closue is not implemented."
-
-            cell.contentConfiguration = contentConfiguration
-        }
-        return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: 0)
-    }
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0x01
-    }
-}
-
 // MARK: `UICollectionViewDelegate`
 
 extension RemindersViewController: UICollectionViewDelegate {
