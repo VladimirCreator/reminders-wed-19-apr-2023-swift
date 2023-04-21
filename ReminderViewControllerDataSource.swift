20dca644eccff094332608f4bf0eb15dcc17c2cc
Vladimir <49140851+VladimirCreator@users.noreply.github.com>
2023-04-21 15:03:09 +0500

    Append `extension RemindersViewController: UICollectionViewDataSource`

--- ReminderViewControllerDataSource.swift
+++ ReminderViewControllerDataSource.swift
@@ -1,1 +1,17 @@
 import UIKit
+
+extension RemindersViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Int> { cell, indexPath, itemIdentifier in
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = "This closue is not implemented."
+
+            cell.contentConfiguration = contentConfiguration
+        }
+        return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: 0)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0x00
+    }
+}
