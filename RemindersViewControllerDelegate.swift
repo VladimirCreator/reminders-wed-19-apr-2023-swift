0456385c45a019a85d1036b30af5c81bbead5111
Vladimir <49140851+VladimirCreator@users.noreply.github.com>
2023-04-21 15:10:03 +0500

    Append `extension RemindersViewController: UINavigationControllerDelegate`

--- RemindersViewControllerDelegate.swift
+++ RemindersViewControllerDelegate.swift
@@ -1,5 +1,15 @@
 import UIKit
 
+// MARK: `UINavigationControllerDelegate`
+
+extension RemindersViewController: UINavigationControllerDelegate {
+    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        // ...
+    }
+}
+
+// MARK: `UICollectionViewDelegate`
+
 extension RemindersViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         // ...
