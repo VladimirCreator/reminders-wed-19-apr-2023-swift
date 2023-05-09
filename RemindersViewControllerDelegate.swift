eb612bc69af44b1be4cbf884b6fa20f486da8e10
Vladimir <49140851+VladimirCreator@users.noreply.github.com>
2023-05-09 06:47:05 +0500

    Mark

--- RemindersViewControllerDelegate.swift
+++ RemindersViewControllerDelegate.swift
@@ -1,23 +1,31 @@
 import UIKit
 
-// MARK: `UINavigationControllerDelegate`
-
-extension RemindersViewController: UINavigationControllerDelegate {
-    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
-        // ...
-    }
-}
-
-// MARK: `UICollectionViewDelegate`
+// MARK:- `UICollectionViewDelegate`
 
 extension RemindersViewController: UICollectionViewDelegate {
+
+    // MARK:- `UICollectionViewDelegate`
+
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         // ...
     }
 }
 
-// MARK: `UITabBarDelegate`
+// MARK:- `UINavigationControllerDelegate`
+
+extension RemindersViewController: UINavigationControllerDelegate {
+
+    // MARK:- `UINavigationControllerDelegate`
+
+    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        // ...
+    }
+}
+
+// MARK: `-UITabBarDelegate`
 
 extension RemindersViewController: UITabBarDelegate {
-    // ...
+
+    // MARK: `-UITabBarDelegate`
+
 }
